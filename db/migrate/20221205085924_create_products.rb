6df850d3f6e90a6ba231fb28dd28d8983e228eb2
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-05 12:04:18 +0300

    create product attributes

--- db/migrate/20221205085924_create_products.rb
+++ db/migrate/20221205085924_create_products.rb
@@ -1,6 +1,11 @@
 class CreateProducts < ActiveRecord::Migration[7.0]
   def change
     create_table :products do |t|
+      t.belongs_to :user
+      t.string :name
+      t.integer :price
+      t.string :imageurl
+      t.string :description
 
       t.timestamps
     end
