392c5d0b2591c291cf1d99ab9411bc4113ac05ee
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-05 12:19:01 +0300

    create signup delet and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   resources :products
-  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  post "/signup", to: "users#create"
+  get "/login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
 end
