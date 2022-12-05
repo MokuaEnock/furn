6251fec77db43b93799584f4b0d48c6ee11ab7e4
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-05 14:11:17 +0300

    install faker gem

--- Gemfile
+++ Gemfile
@@ -14,7 +14,6 @@ gem "puma", "~> 5.0"
 
 gem 'bcrypt', '~> 3.1.7'
 
-
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 # gem "jbuilder"
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_secured_password
+  has_secure_password
   has_many :recipes
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
