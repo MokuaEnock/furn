2ec7b71dc7c2e0490239fc346611393fdc71d42e
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-05 12:52:45 +0300

    index and show methods for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,13 @@
 class ProductsController < ApplicationController
+  def index
+    render json: Product.all, status: :ok
+  end
+
+  def show
+    product = Product.find(params[:id])
+    render json: product, status: :ok
+  end
+
   private
 
   def products_params
