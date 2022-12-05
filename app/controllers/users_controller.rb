61a471a26633493c93d7e46b48d9aa8c541ae3fd
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-05 12:39:28 +0300

    creat euser params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+  def create
+    user = User.create!(user_params)
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :email, :password, :password_confirmation)
+  end
 end
