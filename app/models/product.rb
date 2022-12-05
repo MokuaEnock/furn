0d34afdb27efe1cf3c147a00a5f8379a63202a0f
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-05 12:32:20 +0300

    add validations for product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :description, presence: true, length: { minimum: 20, maximum: 200 }
+
+  belongs_to :user
 end
